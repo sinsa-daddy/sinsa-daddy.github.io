c87f2538fbb40b8eb4e287447d9d31be4e4ca301
HeskeyBaozi <hezhiyu233@foxmail.com>
2024-04-25 15:53:03 +0000

    Deploying to gh-pages from @ sinsa-daddy/sinsa@73a448e0cf8d18e3c7bdc768d3c39c17ac3de224 🚀

--- functions/api-worker/[[catchAll]].js
+++ functions/api-worker/[[catchAll]].js
@@ -7686,7 +7686,7 @@ async function getWBIKeys() {
       "https://api.bilibili.com/x/web-interface/nav",
       {
         headers: {
-          "user-agent": USER_AGENT,
+          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
           referer: "https://www.bilibili.com",
           "Accept-Encoding": "gzip, deflate, br",
           "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en;q=0.3,en-US;q=0.2",
